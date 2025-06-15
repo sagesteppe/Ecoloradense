eecaf33c83e46cbacb106ea6c97c47319bc02bac
sagesteppe <reedbenkendorf27@gmail.com>
2025-06-14 19:41:48 -0500

    euclidean dist between patches

--- scripts/functions.R
+++ scripts/functions.R
@@ -1245,3 +1245,117 @@ patchAttributes <- function(x){
     row.names = FALSE)
   
 }
+
+
+
+patchDist <- function(x, patch_lkp, r_name, thresh_type){
+  # simplify the patches - we will extract only the borders of the patches as 
+  # any migration event would have to pass through these. 
+  patch_borders <- terra::as.polygons(x) |> 
+    sf::st_as_sf() 
+  sf::st_agr(patch_borders) = "constant"
+  
+  patch_borders <- patch_borders |>
+    sf::st_cast('MULTILINESTRING') |>
+    sf::st_cast('LINESTRING') |>
+    dplyr::rename('Patch' = 1)
+  
+  # now convert these lines into segments less than or (equal to) 90m in length #
+  segs <- stplanr::line_segment(patch_borders, segment_length = 180, use_rsgeo = TRUE) |>
+    dplyr::group_by(Patch) |>
+    dplyr::mutate(ID = 1:dplyr::n(), .before = geometry)
+  
+  # from each segment sample one point along the lines length - near the center. 
+  # this gives us control on where we calculate distances to/from.
+  sf::st_agr(segs) = "constant"
+  pts <- sf::st_point_on_surface(segs)
+  
+  # if there are many many points we will sample them to limit them to 100 points per
+  # large polygon. 
+  pts <- pts |>
+    dplyr::group_by(Patch) |>
+    dplyr::slice_sample(n = 100) |>
+    dplyr::ungroup()
+  
+  # we focus on calculating a subset of distances between patches known to 
+  # be occupied and patches not known to be occupied. We will separate the data set 
+  # into the occupied and unoccupied patches. 
+  
+  focal_pts <- dplyr::filter(pts, Patch %in% patch_lkp$patch)
+  nf_pts <- dplyr::filter(pts, !Patch %in% patch_lkp$patch)
+  
+  # calculate all distances from each of these points to to our focal occupied polygons #
+  
+  d_mat <- sf::st_distance(focal_pts, nf_pts)
+  d_mat <- data.frame(apply(d_mat, MARGIN = 2, as.numeric))
+  d_mat <- apply(d_mat, MARGIN = 2, round, 0)
+  
+  colnames(d_mat) <- paste0(nf_pts$Patch, '_', nf_pts$ID)
+  rownames(d_mat) <- paste0(focal_pts$Patch, '_', focal_pts$ID)
+  
+  d_mat <- data.frame(
+    cbind(
+      t(d_mat), 
+      setNames(
+        data.frame(sf::st_coordinates(nf_pts)),
+        c('Longitude', 'Latitude')
+      )
+    )
+  )
+  
+  # prepare data for analysis and to return. 
+  nms <- c('Occ_patch_ID', 'Occ_node_ID')
+  
+  d_long <- d_mat |>
+    
+    # create a long data set 
+    tibble::rownames_to_column('UnOcc_patch') |>
+    tidyr::pivot_longer(cols = starts_with('X'), values_to = 'distance', names_to = 'Occ_patch') |>
+    dplyr::relocate(Longitude:Latitude, .after = last_col()) |>
+    
+    # split apart the patch and node IDs; we only need a fraction of distances between the 
+    # occupied and unoccupied patches to get a sense of their spatial distances. 
+    dplyr::mutate(Occ_patch = stringr::str_remove(Occ_patch, 'X')) |>
+    tidyr::separate_wider_delim(Occ_patch, names = nms, delim = '_') |>
+    tidyr::separate_wider_delim(UnOcc_patch, names = paste0('Un', nms), delim = '_') |>
+    dplyr::mutate(across(.cols = everything(), as.numeric)) |>
+    
+    # reduce the number of patch X patch distances to at most 5 pairs of measurements. 
+    # these theoretically can cover up to 400 meters of edge between both patches. 
+    
+    dplyr::group_by(UnOcc_patch_ID, Occ_patch_ID) |>
+    dplyr::slice_min(order_by = distance, n  = 5) |> 
+    
+    # and make it so results will be in human readable order
+    
+    dplyr::arrange(UnOcc_patch_ID, Occ_patch_ID)
+  
+  # on paper, these measurements may more accurately measure the distance between patches - that is
+  # how far a seed would need to travel on a crows foot to get from one patch to the other
+  
+  # it is useful because most patches are seriously elongated, and getting from the 
+  # centroid of one to the other may be enormous! while the margin(al) habitats may
+  # actually be very close
+  
+  # now using these data, we can identify the single closest linkages in euclidean 
+  # space between patches.
+  min_dist <- d_long |>
+    dplyr::summarise(Min_distance = min(distance), .groups = 'drop_last')
+  
+  mean_dist_all_patches <- d_long |>
+    dplyr::ungroup(Occ_patch_ID) |>
+    dplyr::summarise(
+      Mean_min_distance = mean(distance), 
+      Quant25 = quantile(distance, 0.25), 
+      Quant75 = quantile(distance, 0.25), 
+      .groups = 'drop_last',
+    )
+  
+  fp <- file.path('..', 'results', 'patch_distances')
+  
+  r_name <- paste0(r_name, '-', thresh_type)
+  write.csv(min_dist, file.path(fp, paste0(r_name, '-MinDist.csv')), row.names = F)
+  write.csv(mean_dist_all_patches, file.path(fp, paste0(r_name, '-MeanDist.csv')), row.names = F)
+  write.csv(d_long, file.path(fp, paste0(r_name, '-Distances.csv')), row.names = F)
+  
+}
